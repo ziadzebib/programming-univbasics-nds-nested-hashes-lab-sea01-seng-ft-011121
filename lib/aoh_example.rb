1ba0f069d6d43afec8d8ec14efa24aceccedc90b
Ziad Zebib <zebibziad@gmail.com>
2020-12-30 04:59:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -38,5 +38,5 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
   aoh[row][key] = [new_value]
-  aoh(" ")
+  aoh
 end
