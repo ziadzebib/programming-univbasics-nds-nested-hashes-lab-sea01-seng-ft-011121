7be22ee0c01665e2bbaea2ffb8a3b844c6b408ed
Ziad Zebib <zebibziad@gmail.com>
2020-12-30 04:43:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,10 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  literal_aoh_array = [
+    
+    
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
